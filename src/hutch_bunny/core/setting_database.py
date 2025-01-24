d20b2a34237763844d4501a77cab59e4df6ae962
Jon Couldridge <Jonathan.Couldridge@nottingham.ac.uk>
2025-01-24 10:05:08 +0000

    short db driver expansion now in a function and used by tests (#53)

--- src/hutch_bunny/core/setting_database.py
+++ src/hutch_bunny/core/setting_database.py
@@ -4,6 +4,23 @@ from hutch_bunny.core.db_manager import SyncDBManager, TrinoDBManager
 import hutch_bunny.core.settings as settings
 
 
+def expand_short_drivers(drivername: str):
+    """
+    Expand unqualified "short" db driver names when necessary so we can override sqlalchemy
+    e.g. when using psycopg3, expand `postgresql` explicitly rather than use sqlalchemy's default of psycopg2
+    """
+
+    if drivername == "postgresql":
+        return settings.DEFAULT_POSTGRES_DRIVER
+
+    if drivername == "mssql":
+        return settings.DEFAULT_MSSQL_DRIVER
+
+    # Add other explicit driver qualification as needed ...
+
+    return drivername
+
+
 def setting_database(logger: Logger):
     logger.info("Setting up database connection...")
 
@@ -24,16 +41,9 @@ def setting_database(logger: Logger):
             exit()
     else:
         datasource_db_port = environ.get("DATASOURCE_DB_PORT")
-        datasource_db_drivername = environ.get(
-            "DATASOURCE_DB_DRIVERNAME", settings.DEFAULT_DB_DRIVER
+        datasource_db_drivername = expand_short_drivers(
+            environ.get("DATASOURCE_DB_DRIVERNAME", settings.DEFAULT_DB_DRIVER)
         )
-
-        # expand postgres to a full default driver, so we can override sqlalchemy
-        if datasource_db_drivername == "postgresql":
-            datasource_db_drivername = settings.DEFAULT_POSTGRES_DRIVER
-        
-        if datasource_db_drivername == "mssql":
-            datasource_db_drivername = settings.DEFAULT_MSSQL_DRIVER
 
         try:
             db_manager = SyncDBManager(
