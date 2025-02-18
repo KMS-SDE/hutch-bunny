13cdcd0dc3f8b7a60f53939bfa83166a82da38dc
Andy Rae <github@rae.li>
2025-02-18 18:06:44 +0000

    Fix code distribution query rounding (#91)
    * Fix code distribution query rounding for MSSQL compatibility
    
    * Bump version number

--- src/hutch_bunny/core/solvers/query_solvers.py
+++ src/hutch_bunny/core/solvers/query_solvers.py
@@ -128,7 +128,7 @@ class CodeDistributionQuerySolver(BaseDistributionQuerySolver):
                 if rounding > 0:
                     stmnt = (
                         select(
-                            func.count(table.person_id),
+                            func.round((func.count() / rounding)) * rounding,
                             Concept.concept_id,
                             Concept.concept_name,
                         )
@@ -138,7 +138,7 @@ class CodeDistributionQuerySolver(BaseDistributionQuerySolver):
                 else:
                     stmnt = (
                         select(
-                            func.round((func.count() / rounding)) * rounding,
+                            func.count(table.person_id),
                             Concept.concept_id,
                             Concept.concept_name,
                         )
